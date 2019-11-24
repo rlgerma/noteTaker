f5ca867eaf2390efd00fa56cf8f5d4ac136faeb1
Richard Germaine <rlgerma92@outlook.com>
2019-11-23 21:14:07 -0700

    routes folder also added

---
+++ Develop/routes/htmlRoutes.js
@@ -0,0 +1,19 @@
+const path = require("path");
+
+module.exports = function(app) {
+    app.get(
+        "/",
+        function(req, res) {
+            console.log(`Received a ${req.method} from URL ${req.url}`)
+            res.sendFile(path.join(__dirname, "../public/index.html"));
+        }
+    )
+
+    app.get(
+        "/notes",
+        function(req, res) {
+            console.log(`Received a ${req.method} from URL ${req.url}`)
+            res.sendFile(path.join(__dirname, "../public/notes.html"));
+        }
+    )
+}
